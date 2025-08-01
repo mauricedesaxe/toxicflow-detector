6b4ff2a8db6974d6543c65e3f24f0b01d9e982f8
lazaralex98 <alex@leetsoftware.com>
2025-08-01 14:20:51 +0300

    rename to heuristics
    we'll introduce a simulations-based detection algorithm

--- src/sandwich/mod.rs
+++ src/sandwich/mod.rs
@@ -1,5 +1,7 @@
-pub mod same_block;
+pub mod same_block_heuristics;
+pub mod same_block_sim;
 pub mod types;
 
-pub use same_block::{find_same_block_sandwiches, SandwichAttack};
+pub use same_block_heuristics::{find_same_block_sandwiches, SandwichAttack};
+pub use same_block_sim::{analyze_sandwich_price_impact, PoolState, PriceImpactAnalysis};
 pub use types::SwapTransaction;
