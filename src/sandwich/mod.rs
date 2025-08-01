f3ff7b63403a6a226f8915f4eee0d8c678997cde
lazaralex98 <alex@leetsoftware.com>
2025-08-01 14:14:33 +0300

    move swap transaction out of same_block

--- src/sandwich/mod.rs
+++ src/sandwich/mod.rs
@@ -1,3 +1,5 @@
 pub mod same_block;
+pub mod types;
 
-pub use same_block::{find_same_block_sandwiches, SandwichAttack, SwapTransaction};
+pub use same_block::{find_same_block_sandwiches, SandwichAttack};
+pub use types::SwapTransaction;
